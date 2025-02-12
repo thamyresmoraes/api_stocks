219f39cae9dada73957c1180d37259c1e0abb323
thamyresmoraes <thamyresmoraes@live.com>
2025-02-12 14:36:34 -0300

    main.go

--- main.go
+++ main.go
@@ -1,37 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
+	"stock_api/api"
+	_ "stock_api/docs"
 )
 
-// Estrutura do JSON de resposta
-type Response struct {
-	Message string `json:"message"`
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json") // Define que o retorno é JSON
-	response := Response{Message: "API está rodando!"}
-	json.NewEncoder(w).Encode(response) // Converte a struct para JSON e envia
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-
-	// Força a API a rodar em IPv4
-	listener, err := net.Listen("tcp", "0.0.0.0:8080")
-	if err != nil {
-		log.Fatal("Erro ao iniciar servidor:", err)
-	}
-
-	fmt.Println("✅ Server running on http://0.0.0.0:8080 (IPv4 only)")
-
-	err = http.Serve(listener, nil)
-	if err != nil {
-		log.Fatal("Erro ao rodar servidor:", err)
-	}
+	api.SetupRoutes()
 }
